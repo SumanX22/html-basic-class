9f0e27c65e24f1585c8d4c2b6e6b05f3cefb6997
SumanxX22 <suman.mesaha22@gmail.com>
2023-04-08 22:43:29 +0530

    added server basic endpoints

---
+++ server/server.js
@@ -0,0 +1,53 @@
+// THE MAIN ENTRY POINT FOR THE SERVER
+
+const express = require("express"); //is used to tell the interpreter that this package will be used in this file
+const app = express();
+
+// use JSON parser
+app.use(express.json());
+
+// GET at path = /
+app.get("/", (req, res) => {
+  // database theke bar kore pathabi
+
+  res.status(200).json({
+    name: "Udita Chatterjee",
+    age: 22,
+    nickname: "Ghonchu",
+  });
+});
+
+app.post("/", (req, res) => {
+  const payload = req.body;
+
+  // database a likhe dibi
+
+  res
+    .status(200)
+    .json({ message: "Successfully recieved data!", yourData: payload });
+});
+
+app.put("/", (req, res) => {
+  const payload = req.body;
+
+  // database a likhe dibi
+  res
+    .status(200)
+    .json({ message: "Successfully recieved data!", yourData: payload });
+});
+
+app.delete("/", (req, res) => {
+  const query = req.query;
+
+  res
+    .status(200)
+    .json({ message: "Successfully deleted " + query["name"] + "!" });
+});
+
+// PORT number for the server
+const PORT = 5000;
+
+// STARTS THE SERVER ON YOUR COMPUTER
+app.listen(PORT, () => {
+  console.log("Server start hoye geche at " + PORT);
+});
